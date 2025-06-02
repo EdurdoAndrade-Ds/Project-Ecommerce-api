9a0c6af63a6871ee94de47baeb47066c36bdbca6
Mateus S. Oliveira <92745061+dalik1@users.noreply.github.com>
2025-06-02 17:47:48 -0300

    02.06.2025
    mudanças no docker

--- src/main/java/org/ecommerce/ecommerceapi/product/controller/ProductController.java
+++ src/main/java/org/ecommerce/ecommerceapi/product/controller/ProductController.java
@@ -1,7 +1,6 @@
 package org.ecommerce.ecommerceapi.product.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.ecommerce.ecommerceapi.product.model.Product;
 import org.ecommerce.ecommerceapi.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,29 +8,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.validation.Valid;
+        import jakarta.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/produtos")
+@RequestMapping("/produtos")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    @Operation(summary = "Cria um novo produto")
     @PostMapping
     public ResponseEntity<Product> create(@Valid @RequestBody Product produto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.salvar(produto));
     }
 
-    @Operation(summary = "Lista todos os produtos")
     @GetMapping
     public ResponseEntity<List<Product>> list() {
         return ResponseEntity.ok(productService.buscarTodos());
     }
 
-    @Operation(summary = "Busca um produto pelo ID")
     @GetMapping("/{id}")
     public ResponseEntity<Product> searchForIdProduto(@PathVariable Long id) {
         return productService.buscarPorId(id)
@@ -39,14 +35,12 @@ public class ProductController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @Operation(summary = "Atualiza um produto existente")
     @PutMapping("/{id}")
     public ResponseEntity<Product> updateProduto(@PathVariable Long id, @Valid @RequestBody Product product) {
         product.setId(id);
         return ResponseEntity.ok(productService.atualizar(product));
     }
 
-    @Operation(summary = "Remove um produto pelo ID")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduto(@PathVariable Long id) {
         productService.deletar(id);
