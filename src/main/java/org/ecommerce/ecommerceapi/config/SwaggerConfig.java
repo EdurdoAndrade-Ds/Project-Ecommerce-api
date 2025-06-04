1fff8f9d44a3a99d3d42802fe90ad2d577b7d1e1
Eduardo Andrade dos Santos <91063724+EdurdoAndrade-Ds@users.noreply.github.com>
2025-06-04 16:42:18 -0300

    fix: configurando Swagger para usar URL dinâmica

--- src/main/java/org/ecommerce/ecommerceapi/config/SwaggerConfig.java
+++ src/main/java/org/ecommerce/ecommerceapi/config/SwaggerConfig.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,10 +14,11 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${server.swagger-url}")
+    private String swaggerServerUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
-
-        
         return new OpenAPI()
                 .info(new Info()
                         .title("E-commerce API")
@@ -31,9 +32,8 @@ public class SwaggerConfig {
                                 .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
                 .servers(List.of(
                         new Server()
-                                .url("https://project-ecommerce-api.onrender.com")
-                                .description("Servidor Render")
+                                .url(swaggerServerUrl)
+                                .description("Servidor Configurado")
                 ));
-
     }
-} 
+}
