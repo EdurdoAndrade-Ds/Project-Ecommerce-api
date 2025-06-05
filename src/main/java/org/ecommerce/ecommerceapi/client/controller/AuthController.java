ec41783a2d3da9b82aedf32ce83e1e922180f36a
Eduardo Andrade dos Santos <91063724+EdurdoAndrade-Ds@users.noreply.github.com>
2025-06-05 15:42:47 -0300

    AuthController

--- src/main/java/org/ecommerce/ecommerceapi/client/controller/AuthController.java
+++ src/main/java/org/ecommerce/ecommerceapi/client/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/clientes")
 public class AuthController {
 
     @Autowired
