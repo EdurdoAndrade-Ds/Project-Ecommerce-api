c0c989d9c2eaa3682666503b5f23f0cc185e0222
Eduardo Andrade dos Santos <91063724+EdurdoAndrade-Ds@users.noreply.github.com>
2025-06-09 00:45:37 -0300

    swagger funcionando

--- src/main/java/org/ecommerce/ecommerceapi/security/SecurityConfig.java
+++ src/main/java/org/ecommerce/ecommerceapi/security/SecurityConfig.java
@@ -46,6 +46,16 @@ public class SecurityConfig {
                         .requestMatchers("/api/clientes").permitAll()
                         .requestMatchers("/api/clientes/{id}").permitAll()
                         .requestMatchers("/api/clientes/filtro").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/api-docs/**",
+                                "/webjars/**",
+                                "/v3/api-docs.yaml",
+                                "/swagger-ui/index.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
