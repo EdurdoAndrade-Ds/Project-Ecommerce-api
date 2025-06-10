4e2ce96a9421389d8d9a64c9703f501969778d19
zeguipereira <161784864+zeguipereira@users.noreply.github.com>
2025-06-10 01:01:49 -0300

    import nos testes

--- src/test/java/org/ecommerce/ecommerceapi/TestEcommerceApiApplication.java
+++ src/test/java/org/ecommerce/ecommerceapi/TestEcommerceApiApplication.java
@@ -1,11 +1,15 @@
 package org.ecommerce.ecommerceapi;
 
 import org.springframework.boot.SpringApplication;
+import org.ecommerce.ecommerceapi.EcommerceApiApplication;
+import org.ecommerce.ecommerceapi.TestcontainersConfiguration;
 
 public class TestEcommerceApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(EcommerceApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication
+            .from(EcommerceApiApplication::main)
+            .with(TestcontainersConfiguration.class)
+            .run(args);
     }
-
 }
