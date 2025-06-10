a4b1d828462c4b623d823a89465b3f4e958377f1
Eduardo Andrade dos Santos <91063724+EdurdoAndrade-Ds@users.noreply.github.com>
2025-06-10 01:11:55 -0300

    teste

--- src/test/java/org/ecommerce/ecommerceapi/TestEcommerceApiApplication.java
+++ src/test/java/org/ecommerce/ecommerceapi/TestEcommerceApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestEcommerceApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(EcommerceApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(EcommerceApiApplication::main).with(org.ecommerce.ecommerceapi.TestcontainersConfiguration.class).run(args);
     }
 
 }
