c276f9f5f0ade2174d209a2541746ee2f6f6bc91
zeguipereira <161784864+zeguipereira@users.noreply.github.com>
2025-06-10 00:20:39 -0300

    ajustes no pom

---
+++ src/test/java/src/test/java/org/ecommerce/ecommerceapi/TestcontainersConfiguration.java
@@ -0,0 +1,9 @@
+package org.ecommerce.ecommerceapi;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestcontainersConfiguration {
+    // Você pode adicionar aqui futuras configurações de containers para teste,
+    // mas por enquanto fica vazia apenas para satisfazer o SpringApplication.with(...)
+}
